12f773c5c8a26056cd31284e2bd1ee629cee10ae
Igor.Kirillov <Igor.Kirillov@x5.ru>
2024-09-07 02:00:31 +0300

    commit

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-echo "TASK 1\n";
+echo "TASK 2.1\n";
 
 echo "Current file: ";
 echo __FILE__;
@@ -9,15 +9,66 @@ echo "\n";
 echo "Current row: ";
 echo __LINE__;
 
-echo "\nTASK 2\n";
+echo "\nTASK 2.2\n";
 
 echo <<<TAG
 heredoc text
 TAG;
 
-echo "\nTASK 3\n";
+echo "\nTASK 2.3\n";
 
 $a='Рыба';
 $b='человек';
 
 echo "$a рыбою сыта, а $b человеком";
+
+echo "\nTASK 3.1\n";
+
+//$variable = 3.14;
+ $variable = 3;
+// $variable = 'one';
+// $variable = true;
+// $variable = null;
+// $variable = [];
+
+//  Ваш программный код, в котором переменной $type
+//  присваивается одно из значений: bool, float,
+//  int, string, null или other
+if (is_bool($variable)) {
+    $type = "boolean";
+} else if (is_float($variable)) {
+    $type = "float";
+} else if (is_int($variable)) {
+    $type = "int";
+} else if (is_string($variable)) {
+    $type = "string";
+} else if (is_null($variable)) {
+    $type = "null";
+} else {
+    $type = "other";
+}
+echo "type is $type";
+
+echo "\nTASK 3.2\n";
+
+switch ($variable) {
+    case is_bool($variable):
+        $type = "boolean";
+        break;
+    case is_float($variable):
+        $type = "float";
+        break;
+    case is_int($variable):
+        $type = "int";
+        break;
+    case is_string($variable):
+        $type = "string";
+        break;
+    case is_null($variable):
+        $type = "null";
+        break;
+    default:
+        $type = "other";
+}
+
+echo "type is $type";
